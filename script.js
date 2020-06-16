4f5aeff309d4478de17b9e91e137bf49ccd68f0d
Nam <62420404+HoangNamDo@users.noreply.github.com>
2020-06-16 16:05:24 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-function convert(degree) {
-  let num;
-  if (degree == "fahrenheitBox") {
-    num = (document.getElementById("fahrenheitBox").value - 32) * (5 / 9);
-    document.getElementById("celsiusBox").value = Math.round(num * 100) / 100;
-    if (document.getElementById("fahrenheitBox").value == '') {
-      document.getElementById("celsiusBox").value = '';
-    }
-  }
-  else {
-    num = (document.getElementById("celsiusBox").value * (9 / 5)) + 32;
-    document.getElementById("fahrenheitBox").value = Math.round(num * 100) / 100;
-    if (document.getElementById("celsiusBox").value == '') {
-      document.getElementById("fahrenheitBox").value = '';
-    }
+function convertFahrenheitToCelsius() {
+  let fahrenheitValue = (document.getElementById("fahrenheitBox").value - 32) * (5 / 9);
+  document.getElementById("celsiusBox").value = Math.round(fahrenheitValue * 100) / 100;
+  if (document.getElementById("fahrenheitBox").value == '') {
+    document.getElementById("celsiusBox").value = '';
   }
 }
+
+function convertCelsiusToFahrenheit() {
+  let celsiusValue = (document.getElementById("celsiusBox").value * (9 / 5)) + 32;
+  document.getElementById("fahrenheitBox").value = Math.round(celsiusValue * 100) / 100;
+  if (document.getElementById("celsiusBox").value == '') {
+    document.getElementById("fahrenheitBox").value = '';
+  }
+
+}
+
