44af46a782fa497be19838f91813461e09b481d4
Nam <62420404+HoangNamDo@users.noreply.github.com>
2020-06-16 15:51:27 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-fahrenheitPlaceholder.addEventListener("onkeyup", fahrenheitToCelsius("fahrenheitPlaceholder", "celsiusPlaceholder"));
-celsiusPlaceholder.addEventListener("onkeyup", celsiusToFahrenheit("celsiusPlaceholder", "fahrenheitPlaceholder"));
-
-function fahrenheitToCelsius(sourceElementId, destinationElementId) {
-    if (document.getElementById("sourceElementId").value == "") {
-        document.getElementById("destinationElementId").value = "";
-    } else {
-        let fahrenheitValue = document.getElementById(sourceElementId).value;
-        celsiusValue = (5 / 9) * (fahrenheitValue - 32);
-        document.getElementById(destinationElementId).value = celsiusValue;
+function convert(degree) {
+  let num;
+  if (degree == "fahrenheitBox") {
+    num = (document.getElementById("fahrenheitBox").value - 32) * (5 / 9);
+    document.getElementById("celsiusBox").value = Math.round(num * 100) / 100;
+    if (document.getElementById("fahrenheitBox").value == '') {
+      document.getElementById("celsiusBox").value = '';
     }
-}
-
-function celsiusToFahrenheit(sourceElementId, destinationElementId) {
-    if (document.getElementById("sourceElementId").value == "") {
-        document.getElementById("destinationElementId").value = "";
-    } else {
-        let celsiusValue = document.getElementById(sourceElementId).value;
-        fahrenheitValue = celsiusValue * (9 / 5) + 32;
-        document.getElementById(destinationElementId).value = fahrenheitValue;
+  }
+  else {
+    num = (document.getElementById("celsiusBox").value * (9 / 5)) + 32;
+    document.getElementById("fahrenheitBox").value = Math.round(num * 100) / 100;
+    if (document.getElementById("celsiusBox").value == '') {
+      document.getElementById("fahrenheitBox").value = '';
     }
+  }
 }
